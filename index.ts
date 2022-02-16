5551e3b752be0889d4a60f2eb82ddeee126fb676
Besim <besimsokoli10@gmail.com>
2022-02-16 17:45:01 +0100

    added patch resource

--- index.ts
+++ index.ts
@@ -3,7 +3,9 @@ import cors from 'cors'
 const app = express()
 const PORT = 8000
 
+app.use(express.json())
 app.use(cors())
+
 type Quote = {
     id: number
     firstName: string
@@ -11,14 +13,16 @@ type Quote = {
     photo: string
     age: number
     text: string
+    likes: number
 }
-const quotes: Quote[] = [
+let quotes: Quote[] = [
     {
         id: Math.random(),
         firstName: 'Nelson',
         lastName: 'Mandela',
         photo: 'https://mir-s3-cdn-cf.behance.net/project_modules/fs/a4b9c726714513.56359606a87fc.jpg',
         age: 95,
+        likes: 12,
         text: 'Do not judge me by my successes, judge me by how many times I fell down and got back up again.'
     },
     {
@@ -27,6 +31,7 @@ const quotes: Quote[] = [
         lastName: 'Armour',
         photo: 'https://images.gr-assets.com/authors/1338769201p8/205834.jpg',
         age: 82,
+        likes: 12,
         text: 'Politics, it seems to me, for years, or all too long, has been concerned with right or left instead of right or wrong.'
     },
     {
@@ -35,6 +40,7 @@ const quotes: Quote[] = [
         lastName: '',
         photo: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTY2ODIwNDI5NjA2OTU0OTg1/aristotle--getty.jpg',
         age: 62,
+        likes: 12,
         text: 'Democracy arises out of the notion that those who are equal in any respect are equal in all respects; because men are equally free, they claim to be absolutely equal.'
     },
     {
@@ -42,6 +48,7 @@ const quotes: Quote[] = [
         lastName: 'Churchill',
         photo: 'https://www.madametussauds.com/wien/media/ge2b0w0o/winston-churchill-min.jpg?center=0.15733997428394192,0.47833333333333333&mode=crop&width=700&height=700',
         age: 90,
+        likes: 12,
         text: 'The inherent vice of capitalism is the unequal sharing of blessings; the inherent virtue of socialism is the equal sharing of miseries.'
     },
     {
@@ -49,6 +56,7 @@ const quotes: Quote[] = [
         lastName: 'Orwell',
         photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/George_Orwell%2C_c._1940_%2841928180381%29.jpg/1200px-George_Orwell%2C_c._1940_%2841928180381%29.jpg',
         age: 46,
+        likes: 12,
         text: 'Political language… is designed to make lies sound truthful and murder respectable, and to give an appearance of solidity to pure wind.'
     },
     {
@@ -56,6 +64,7 @@ const quotes: Quote[] = [
         lastName: 'Adams',
         photo: 'https://upload.wikimedia.org/wikipedia/commons/8/81/Franklin_P._Adams.jpg',
         age: 78,
+        likes: 12,
         text: 'When the political columnists say Every thinking man they mean themselves, and when candidates appeal to Every intelligent voter they mean everybody who is going to vote for them.'
     },
     {
@@ -63,6 +72,7 @@ const quotes: Quote[] = [
         lastName: 'Roosevelt',
         photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/FDR_1944_Color_Portrait.jpg/1200px-FDR_1944_Color_Portrait.jpg',
         age: 63,
+        likes: 12,
         text: 'In politics, nothing happens by accident. If it happens, you can bet it was planned that way.'
     },
     {
@@ -70,6 +80,7 @@ const quotes: Quote[] = [
         lastName: '',
         photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Plato_Silanion_Musei_Capitolini_MC1377.jpg/1200px-Plato_Silanion_Musei_Capitolini_MC1377.jpg',
         age: 81,
+        likes: 12,
         text: 'Mankind will never see an end of trouble until lovers of wisdom come to hold political power, or the holders of power become lovers of wisdom.'
     },
     {
@@ -77,6 +88,7 @@ const quotes: Quote[] = [
         lastName: 'Einstein',
         photo: 'https://thedecisionlab.com/wp-content/uploads/2020/11/albert-einstein.jpg',
         age: 76,
+        likes: 12,
         text: 'An empty stomach is not a good political advisor.'
     },
     {
@@ -85,6 +97,7 @@ const quotes: Quote[] = [
         lastName: 'Reagan',
         photo: 'https://m.media-amazon.com/images/M/MV5BMGZjNDNlMGYtZWY4MS00MjhmLWIwMWEtYjk3Y2UzNTQ1YzJjXkEyXkFqcGdeQXVyNjUxMjc1OTM@._V1_.jpg',
         age: 93,
+        likes: 12,
         text: 'Government exists to protect us from each other. Where government has gone beyond its limits is in deciding to protect us from ourselves.'
     },
     {
@@ -93,6 +106,7 @@ const quotes: Quote[] = [
         lastName: 'Bonaparte',
         photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Jacques-Louis_David_-_The_Emperor_Napoleon_in_His_Study_at_the_Tuileries_-_Google_Art_Project.jpg/1200px-Jacques-Louis_David_-_The_Emperor_Napoleon_in_His_Study_at_the_Tuileries_-_Google_Art_Project.jpg',
         age: 51,
+        likes: 12,
         text: 'In politics stupidity is not a handicap.'
     },
     {
@@ -101,6 +115,7 @@ const quotes: Quote[] = [
         lastName: 'Rosten',
         photo: 'https://images.gr-assets.com/authors/1290229826p8/59780.jpg',
         age: 88,
+        likes: 12,
         text: 'A conservative is one who admires radicals centuries after they re dead.'
     },
     {
@@ -109,6 +124,7 @@ const quotes: Quote[] = [
         lastName: 'Thatcher',
         photo: 'https://upload.wikimedia.org/wikipedia/commons/3/3d/Margaret_Thatcher_stock_portrait_%28cropped%29.jpg',
         age: 87,
+        likes: 12,
         text: 'In politics, if you want anything said, ask a man; if you want anything done, ask a woman.'
     },
     {
@@ -117,6 +133,7 @@ const quotes: Quote[] = [
         lastName: 'Roosevelt',
         photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/FDR_1944_Color_Portrait.jpg/1200px-FDR_1944_Color_Portrait.jpg',
         age: 63,
+        likes: 12,
         text: 'Let us never forget that government is ourselves and not an alien power over us. The ultimate rulers of our democracy are not a President and senators and congressmen and government officials, but the voters of this country.'
     },
     {
@@ -125,6 +142,7 @@ const quotes: Quote[] = [
         lastName: 'Gross',
         photo: 'https://static01.nyt.com/images/2013/09/03/us/GROSS-obit/GROSS-obit-superJumbo.jpg',
         age: 88,
+        likes: 12,
         text: 'We live in a world in which politics has replaced philosophy.'
     },
     {
@@ -133,6 +151,7 @@ const quotes: Quote[] = [
         lastName: 'Obama',
         photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/President_Barack_Obama.jpg/1200px-President_Barack_Obama.jpg',
         age: 60,
+        likes: 12,
         text: 'When our government is spoken of as some menacing, threatening, foreign entity, it ignores the fact that in our democracy, government is us.'
     },
     {
@@ -141,6 +160,7 @@ const quotes: Quote[] = [
         lastName: 'Ameringer',
         photo: 'https://upload.wikimedia.org/wikipedia/commons/1/13/Oscar_Ameringer.jpg',
         age: 73,
+        likes: 12,
         text: 'Politics is the gentle art of getting votes from the poor and campaign funds from the rich, by promising to protect each from the other.'
     },
     {
@@ -149,6 +169,7 @@ const quotes: Quote[] = [
         lastName: 'Benn',
         photo: 'https://4.bp.blogspot.com/-0Mf6OESW2Ek/WfO8DM4E4tI/AAAAAAAAHNE/Mf1O28h3FYYY1Fr6r0dS1Ed0gDeQihBpgCLcBGAs/s1600/2297225957_1a5775fe8a%255B1%255D_0.jpg',
         age: 78,
+        likes: 12,
         text: 'Politics is the art of looking for trouble, finding it whether it exists or not, diagnosing it incorrectly, and applying the wrong remedy.'
     },
     {
@@ -157,6 +178,7 @@ const quotes: Quote[] = [
         lastName: 'Churchill',
         photo: 'https://www.madametussauds.com/wien/media/ge2b0w0o/winston-churchill-min.jpg?center=0.15733997428394192,0.47833333333333333&mode=crop&width=700&height=700',
         age: 90,
+        likes: 12,
         text: 'Some men change their party for the sake of their principles; others their principles for the sake of their party.'
     },
     {
@@ -165,6 +187,7 @@ const quotes: Quote[] = [
         lastName: 'Churchill',
         photo: 'https://www.madametussauds.com/wien/media/ge2b0w0o/winston-churchill-min.jpg?center=0.15733997428394192,0.47833333333333333&mode=crop&width=700&height=700',
         age: 90,
+        likes: 12,
         text: 'Politics are almost as exciting as war, and quite as dangerous. In war you can only be killed once, but in politics many times.'
     }
 ]
@@ -243,6 +266,7 @@ app.post('/quotes', (req, res) => {
         firstName: firstName,
         lastName: lastName,
         photo: photo,
+        likes: 0,
         age: age,
         text: text,
       };
@@ -254,6 +278,37 @@ app.post('/quotes', (req, res) => {
       res.status(400).send({ errors: errors });
     }
   });
+  app.patch('/quotes/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const { text, firstName, lastName, photo, age, likes  } = req.body;
+  
+    const match = quotes.find(quote => quote.id === id);
+   
+    if (match) {
+      if (typeof text === 'string') match.text = text;
+      if (typeof firstName === 'string') match.firstName = firstName
+      if (typeof lastName === 'string') match.lastName = lastName
+      if (typeof photo === 'string') match.photo = photo
+      if (typeof age === 'number') match.age = age
+      if (typeof likes === 'number') match.likes = likes
+
+      res.send(match);
+    } else {
+      res.status(404).send({ error: 'No quote found with that id.' });
+    }
+  });
+  app.delete('/quotes/:id', (req,res) => {
+    const id = Number(req.params.id);
+    const match = quotes.find(quote => quote.id === id)
+
+    if(match){
+        quotes = quotes.filter(quote => quote.id !== id)
+        res.send({message: 'Quote deleted succesfully!'})
+    }
+    else{
+        res.status(404).send({error: 'Quote not found!'})
+    }
+  })
   app.listen(PORT, () =>{
     console.log(`Server running in port ${PORT}`)
     console.log(`http://localhost:${PORT}`)
